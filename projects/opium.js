2d2f1caeebf8be089049764e86ac068a245d1309
RiccardoBiosas <rbios@protonmail.com>
2021-06-25 18:14:58 +0200

    added polygon and bsc strategies for opium

--- projects/opium.js
+++ projects/opium.js
@@ -9,8 +9,51 @@ const geckoKeys = require('./config/keys.js').keys
 const utils = require('./helper/utils');
 
 
-async function fetch() {
-  var opium = await utils.fetchURL('https://static.opium.network/data/opium-addresses.json');
+async function eth() {
+  const opium = await utils.fetchURL('https://static.opium.network/data/opium-addresses.json');
+
+
+  let tokens = opium.data.tokens;
+  let keys = '';
+  await Promise.all(
+    tokens.map(async (token) => {
+      if (geckoKeys[token.toLowerCase()]) {
+        if (geckoKeys[token.toLowerCase()] !== 'stable') {
+          keys += geckoKeys[token.toLowerCase()]+','
+        }
+      } else {
+
+      }
+    })
+  )
+
+  keys = keys.slice(0, -1)
+  let price_feed = await utils.getPricesfromString(keys)
+  let tvl = 0;
+  await Promise.all(
+    opium.data.contracts.map(async (contract) => {
+      await Promise.all(
+        tokens.map(async (token) => {
+          if (geckoKeys[token.toLowerCase()]) {
+            let balance = await utils.returnBalance(token, contract);
+            let price = 0;
+            if (geckoKeys[token.toLowerCase()] == 'stable') {
+              price = 1
+            } else {
+              price = price_feed.data[geckoKeys[token.toLowerCase()]].usd
+            }
+            tvl += price * balance
+          }
+        })
+      )
+    })
+  )
+
+  return tvl;
+}
+
+async function polygon() {
+  const opium = await utils.fetchURL('https://static.opium.network/data/opium-addresses-polygon.json');
 
 
   let tokens = opium.data.tokens;
@@ -49,9 +92,58 @@ async function fetch() {
   )
 
   return tvl;
-  //return totalTvl.data.totalTvl.tvlInUsd;
+}
+
+async function bsc() {
+  const opium = await utils.fetchURL('https://static.opium.network/data/opium-addresses-bsc.json');
+
+
+  let tokens = opium.data.tokens;
+  let keys = '';
+  await Promise.all(
+    tokens.map(async (token) => {
+      if (geckoKeys[token.toLowerCase()]) {
+        if (geckoKeys[token.toLowerCase()] !== 'stable') {
+          keys += geckoKeys[token.toLowerCase()]+','
+        }
+      } else {
+
+      }
+    })
+  )
+  keys = keys.slice(0, -1)
+  let price_feed = await utils.getPricesfromString(keys)
+  let tvl = 0;
+  await Promise.all(
+    opium.data.contracts.map(async (contract) => {
+      await Promise.all(
+        tokens.map(async (token) => {
+          if (geckoKeys[token.toLowerCase()]) {
+            let balance = await utils.returnBalance(token, contract);
+            let price = 0;
+            if (geckoKeys[token.toLowerCase()] == 'stable') {
+              price = 1
+            } else {
+              price = price_feed.data[geckoKeys[token.toLowerCase()]].usd
+            }
+            tvl += price * balance
+          }
+        })
+      )
+    })
+  )
+
+  return tvl;
 }
 
 module.exports = {
-  fetch
+  ethereum: {
+    tvl: eth
+  },
+  polygon: {
+    tvl: polygon
+  },
+  bsc: {
+    tvl: bsc
+  }
 }
